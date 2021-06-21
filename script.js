1abb5b433a6aae1b0515e01063f96dd4008aea4d
Kyle Ortiz <kyleaortiz@gmail.com>
2021-06-20 19:51:51 -0500

    myStory 2.0

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 h2.textContent = "This content added by JavaScript";
 document.querySelector('body').appendChild(h2);*/
 
-function revealStory() {
-     document.getElementById("hiddenStory").style.display = "block";
-}
+// function revealStory() {
+//      document.getElementById("hiddenStory").style.display = "block";
+// }
+const button = document.querySelector('button') 
+const myStory = document.querySelector('.myStory')  
+
+button.addEventListener('click', function(){   
+     myStory.classList.toggle('is-active')  
+});
